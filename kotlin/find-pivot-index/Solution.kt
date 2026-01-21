d429d11b3cdb74887857745ef2683a398f170c5f
David Olivares <david.olivares.dev@gmail.com>
2026-01-20 21:39:41 -0800

    feat: Adds solution for Find Pivot Index

---
+++ kotlin/find-pivot-index/Solution.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun pivotIndex(nums: IntArray): Int {
+        var totalSum = 0
+
+        for (num in nums) {
+            totalSum += num
+        }
+
+        var leftSum = 0
+
+        for (i in nums.indices) {
+            val rightSum = totalSum - leftSum - nums[i]
+
+            if (leftSum == rightSum) {
+                return i
+            }
+
+            leftSum += nums[i]
+        }
+
+        return -1
+    }
+}
